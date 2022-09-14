2eebd245bcdb8c535532b7747c153e85407445db
Igoche John <igochej4@gmail.com>
2022-09-14 10:26:04 +0100

    modified the main.js file
    Added new comments to the main.js file

--- main.js
+++ main.js
@@ -7,7 +7,10 @@
 
   //Using a forEach (High Order Array Method) to loop through every button and assign place it on the screen when clicked
   buttons.forEach((button) => {
+    // makes the calculator responsive or better still sensitive to any 'click' events
+    // I decided to use an arrow function which makes the whole line of simpler and short, easy to understand as well
     button.addEventListener("click", function (e) {
+      //Takes the assigned value data-num as seen in our index.html and passes it directly unto our calculator screen
       let value = e.target.dataset.num;
       screen.value += value;
     });
